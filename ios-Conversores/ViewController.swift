fc9a75e1d4c519d6e1e2526e69f03d4ed69d08ef
pc <adrielfreitasalves@gmail.com>
2018-07-04 19:58:35 -0300

    modificando e criando metodos no toryboard

--- ios-Conversores/ViewController.swift
+++ ios-Conversores/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tfValue: UITextField!
+    @IBOutlet weak var btUnit1: UIButton!
+    @IBOutlet weak var btUnit2: UIButton!
+    @IBOutlet weak var lbResult: UILabel!
+    @IBOutlet weak var lbResultUnit: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +27,12 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func showNext(_ sender: UIButton) {
+    }
 
-
+    @IBAction func convert(_ sender: UIButton) {
+    }
+    
 }
 
